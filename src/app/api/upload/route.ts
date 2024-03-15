79e027d270db3fba36bc38e6777104160d0a2dc8
Colby Fayock <fayock@gmail.com>
2024-03-14 21:36:15 -0400

    fixing tagging in upload route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -13,7 +13,10 @@ export async function POST(request: Request) {
   const tags = requestFormData.getAll('tags') as Array<string> || [];
 
   const uploadOptions: Record<string, string | Array<string> | boolean> = {
-    tags: [String(process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_TAG), ...tags],
+    tags: [
+
+      ...tags,
+    ],
     folder: String(process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_FOLDER)
   };
 
@@ -21,11 +24,28 @@ export async function POST(request: Request) {
     // We're already including the folder in the above options, if the public ID
     // includes the folder in it as well, we need to strip it, otherwise the
     // upload will be attempted to be placed in folder/folder
+
     uploadOptions.public_id = publicId.replace(`${process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_FOLDER}/`, '');
     uploadOptions.overwrite = true;
     uploadOptions.invalidate = true;
+
+    // If the upload request is explicitly defining a set of tags, still apply these
+    // in the event they are intended to overwrite the existing tags
+
+    if ( tags ) {
+      uploadOptions.tags = tags;
+    }
+  } else {
+    // We only want to add tags if we're uploading a new image to avoid overwriting existing
+    // tags on an image, which would presumably have the standard tags already applied
+
+    uploadOptions.tags = [
+      String(process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_TAG),
+      String(process.env.NEXT_PUBLIC_CLOUDINARY_LIBRARY_TAG),
+      ...tags,
+    ]
   }
-  
+
   const results = await cloudinary.uploader.upload(file, uploadOptions);
 
   return Response.json(results);
