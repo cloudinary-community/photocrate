82ccbedeb64a16ad10ac1fc50c25b2c8a2af99cd
Colby Fayock <fayock@gmail.com>
2024-03-26 15:56:32 -0300

    adding deploy button, starting getting started

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,13 +8,13 @@ import { Button } from "@/components/ui/button"
 export default async function Home() {
   return (
     <div>
-      <Container className="max-w-6xl grid text-center md:text-left md:grid-cols-[3fr_4fr] gap-4 md:gap-8 md:py-12 my-12">
+      <Container className="max-w-6xl grid text-center md:text-left md:grid-cols-[3fr_4fr] gap-4 md:gap-8 md:py-12 mt-12 mb-16">
         <div className="flex flex-col justify-center">
           <h1 className="text-4xl md:text-5xl font-black mb-5">
             Photobox
           </h1>
-          <p className="mb-6">
-            Text
+          <p className="text-lg mb-6">
+            Image library and interactive editor built with Next.js
           </p>
           <p className="mb-6">
             <Button>
@@ -37,7 +37,7 @@ export default async function Home() {
         <h2 className="text-2xl md:text-3xl font-bold mb-6">
           Create a new Photobox
         </h2>
-        <div className="grid sm:grid-cols-[auto_1fr] gap-2 max-w-lg mx-auto">
+        <div className="grid sm:grid-cols-[auto_1fr] gap-2 max-w-lg mx-auto mb-6">
           <code className="flex items-center overflow-x-scroll h-12 text-sm border border-zinc-200 py-3 px-4 rounded">
             <pre>{`npx create-next-app@latest https://github.com/cloudinary-community/photobox photobox`}</pre>
           </code>
@@ -46,6 +46,14 @@ export default async function Home() {
             text="npx create-next-app@latest https://github.com/cloudinary-community/photobox photobox"
           />
         </div>
+        <p className="text-sm mb-6">
+          Or deploy your new library!
+        </p>
+        <p>
+          <a className="inline-block" href="https://vercel.com/new/clone?demo-description=Photo%20library%20and%20interactive%20editor%20built%20with%20Next.js&demo-image=https%3A%2F%2Fres.cloudinary.com%2Fphotoboxdev%2Fimage%2Fupload%2Ff_auto%2Fq_auto%2Fassets%2Fvercel-deploy-photobox-media-library_yclazk&demo-title=Photobox&demo-url=https%3A%2F%2Fdemo.photobox.dev%2F&project-name=Photobox&repository-name=photobox&repository-url=https%3A%2F%2Fgithub.com%2Fcloudinary-community%2Fphotobox">
+            <img width="103" height="32" src="https://vercel.com/button" alt="Deploy to Vercel" />
+          </a>
+        </p>
       </Container>
     </div>
   )
--- src/app/(pages)/getting-started/page.tsx
+++ src/app/(pages)/getting-started/page.tsx
@@ -4,11 +4,11 @@ export default async function Home() {
   return (
     <main className="py-12">
       <Container className="max-w-6xl">
-        <div className="flex flex-col justify-center">
-          <h1 className="text-5xl font-black mb-5">
+        <div className="prose mx-auto">
+          <h1>
             Getting Started
           </h1>
-          <p className="mb-6">
+          <p>
             Text
           </p>
         </div>
