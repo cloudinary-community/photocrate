9e737553a6ebc7b4618ad4b15f1fcd6bc6e7cdd2
Colby Fayock <fayock@gmail.com>
2024-03-27 10:13:10 -0300

    homepage

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import Container from '@/components/Container';
 
 const Footer = () => {
   return (
-    <footer className="mt-20">
+    <footer className="bg-primary text-blue-100">
       <Container className="p-6">
-        <p className="text-center text-zinc-500">
+        <p className="text-center">
           Built with <a className="underline font-medium text-inherit" href="https://cloudinary.com/">Cloudinary</a> &amp; <a className="underline font-medium text-inherit" href="https://nextjs.org/">Next.js</a> by <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Colby Fayock</a>
         </p>
       </Container>
