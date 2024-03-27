ec6e5ca89769910e6fba5b0163f478c80e3a1935
Colby Fayock <fayock@gmail.com>
2024-03-27 13:28:28 -0300

    theme

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -10,7 +10,7 @@ export default function HomeLayout({
     <div className="h-full grid grid-rows-[auto_1fr_auto]">
       <Nav />
       { children }
-      <Footer />
+      <Footer className="mt-16" />
     </div>
   )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,10 +8,10 @@ interface FooterProps {
 
 const Footer = ({ className }: FooterProps) => {
   return (
-    <footer className={cn('bg-[#FABD36] text-[#6E4C00]', className)}>
+    <footer className={cn('bg-zinc-900 text-zinc-400', className)}>
       <Container className="p-6">
         <p className="text-center">
-          Built with <a className="underline font-medium text-inherit" href="https://cloudinary.com/">Cloudinary</a> &amp; <a className="underline font-medium text-inherit" href="https://nextjs.org/">Next.js</a> by <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Colby Fayock</a>
+          &copy; { new Date().getFullYear() } <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Colby Fayock</a>
         </p>
       </Container>
     </footer>
