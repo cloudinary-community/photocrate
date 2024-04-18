e235f4cb21ff29d60174111a6bd00d29ba465885
Colby Fayock <fayock@gmail.com>
2024-04-18 16:52:17 -0300

    edge runtime with manual upload

--- src/app/api/creations/color-pop/route.ts
+++ src/app/api/creations/color-pop/route.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from 'next/server'
-import { v2 as cloudinary } from 'cloudinary';
 import { getCldImageUrl } from 'next-cloudinary';
 
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+import { createHashFromString } from '@/lib/utils';
+
+export const runtime = 'edge';
 
 export async function POST(request: Request) {
   const { publicId } = await request.json()
@@ -35,24 +32,57 @@ export async function POST(request: Request) {
 
   await checkStatus(backgroundRemovedUrl);
 
-  const uploadOptions: Record<string, string | Array<string> | boolean> = {
-    tags: ['background-removed', `original-${publicId}`]
-  };
 
-  const backgroundRemovedResource = await cloudinary.uploader.upload(backgroundRemovedUrl, uploadOptions);
+  const timestamp = Date.now();
 
-  const creationUrl = getCldImageUrl({
-    src: publicId,
-    version: Date.now(),
-    grayscale: true,
-    overlays: [{
-      publicId: backgroundRemovedResource.public_id,
-      width: '1.0',
-      flags: ['relative']
-    }]
+  const formData = new FormData();
+
+  const parameters: Record<string, string> = {
+    // tags: ['background-removed', `original-${publicId}`]
+  }
+
+  Object.keys(parameters).sort().forEach(key => {
+    formData.append(key, String(parameters[key]));
   });
 
-  return NextResponse.json({
-    url: creationUrl
-  });
+  const paramsString = Object.keys(parameters).map(key => `${key}=${parameters[key]}`).join('&');
+
+  // const paramsHash = await createHashFromString(`${paramsString}&timestamp=${timestamp}${process.env.CLOUDINARY_API_SECRET}`);
+  const paramsHash = await createHashFromString(`timestamp=${timestamp}${process.env.CLOUDINARY_API_SECRET}`);
+
+  formData.append('file', backgroundRemovedUrl);
+  formData.append('api_key', String(process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY));
+  formData.append('timestamp', String(timestamp));
+  formData.append('signature', paramsHash);
+
+  try {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
+      method: 'POST',
+      body: formData
+    }).then(r => r.json());
+
+    if ( response.error ) {
+      throw new Error(response.error?.message);
+    }
+
+    const creationUrl = getCldImageUrl({
+      src: publicId,
+      version: Date.now(),
+      grayscale: true,
+      overlays: [{
+        publicId: response.public_id,
+        width: '1.0',
+        flags: ['relative']
+      }]
+    });
+  
+    return NextResponse.json({
+      url: creationUrl
+    });
+  } catch(e) {
+    console.log('e', e)
+    return new Response(JSON.stringify({ error: e }), {
+      status: 500
+    })
+  }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -78,3 +78,17 @@ export function formatBytes(bytes: number) {
 
   return `${amount} ${numberFormat}`;
 }
+
+/**
+ * createHashFromString
+ * @via https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+ */
+
+export async function createHashFromString(data: any, algorithm = 'SHA-256') {
+  if (!data) throw new Error('Failed to create hash. Data undefined.');
+  const encoder = new TextEncoder();
+  const hashBuffer = await crypto.subtle.digest(algorithm, encoder.encode(data))
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+  return hashHex;
+}
