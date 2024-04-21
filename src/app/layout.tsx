c176cb562301770aa73fb605375f4551f7ac289d
Colby Fayock <fayock@gmail.com>
2024-04-20 23:21:17 -0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { getCldOgImageUrl } from 'next-cloudinary';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} h-screen`}>
         { children }
+        <Analytics />
       </body>
     </html>
   )
